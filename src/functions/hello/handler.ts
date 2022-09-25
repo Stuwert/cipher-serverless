1a5fdad50451caf1b1a2bbb9f48d0d9262a51f6d
Stuart Urback <stuart@urback.net>
2022-09-24 23:23:27 -0600

    Remove consoles and add hello to direct invoke path

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -7,8 +7,6 @@ import schema from "./schema";
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
-  console.log(event);
-  console.log("hello");
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
